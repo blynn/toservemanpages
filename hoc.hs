689e24d95f411937c09674d17f474e429ca56b14
Ben Lynn <benlynn@gmail.com>
2020-07-02 11:06:51 -0700

    hoc: serve files in current directory.

---
+++ hoc.hs
@@ -0,0 +1,31 @@
+-- Webserver serving from current directory.
+
+{-# LANGUAGE OverloadedStrings #-}
+import Data.Binary.Builder
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
+import Data.List (isSuffixOf)
+import Control.Exception (try, displayException, SomeException)
+import Network.Wai
+import Network.Wai.Handler.Warp (runEnv)
+import Network.HTTP.Types
+
+toFilename :: String -> String
+toFilename s = case dropWhile ('/' ==) s of
+  "" -> "index.html"
+  f -> f
+
+main :: IO ()
+main = runEnv 3000 $ \req f -> case requestMethod req of
+  "GET" -> do
+    putStrLn $ show (remoteHost req) ++ " " ++ show (rawPathInfo req)
+    let filename = toFilename $ map (toEnum . fromEnum) $ BS.unpack $ rawPathInfo req
+    r <- try $ BL.readFile filename
+    f $ case r of
+      Left ex -> responseBuilder status404 [] $ putStringUtf8 $ displayException (ex :: SomeException)
+      Right s -> responseBuilder status200 (conTyp filename $ []) $ fromLazyByteString s
+  _ -> f $ responseBuilder status200 [] "OK"
+  where
+  conTyp filename
+    | isSuffixOf ".wasm" filename = ((hContentType, "application/wasm"):)
+    | otherwise = id
